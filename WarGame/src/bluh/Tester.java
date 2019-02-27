f75c12720d683a3f2984a43ada85718ff45bae1b
AldenChiu <aldenchiu13@gmail.com>
2019-02-27 09:47:43 -0700

    Commit

---
+++ WarGame/src/bluh/Tester.java
@@ -0,0 +1,8 @@
+package bluh;
+
+public class Tester {
+	public static void main(String args[])
+	{
+		System.out.println(");alydhvumewvflnhsfd");
+	}
+}
---
+++ WarGame/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ac022868
+ *
+ */
+module WarGame {
+}
